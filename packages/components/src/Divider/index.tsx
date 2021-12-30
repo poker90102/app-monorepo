606233269b6544061cbb549ec5207db0a4ae802c
poker90102 <poker.king90102@gmail.com>
2021-12-30 14:31:19 +0800

    fix lint issue

--- packages/components/src/Divider/index.tsx
+++ packages/components/src/Divider/index.tsx
@@ -3,7 +3,7 @@ import React, { ComponentProps, FC } from 'react';
 import { Divider as NBDivider } from 'native-base';
 
 const Divider: FC<ComponentProps<typeof NBDivider>> = (props) => (
-  <NBDivider bg="divider" {...props} />
+  <NBDivider bg="border-subdued" {...props} />
 );
 
 export default Divider;
